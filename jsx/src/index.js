58255b6e45db03aa0c71a7bdf87360409b73e925
keenoro <kzkho1@student.monash.edu>
2019-10-10 20:34:34 +1100

    Change HTML to JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,9 +2,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Create a react component (JSX)
+// Create a react component (JSX)\
+
+// Tips
+// HTML -> <button style="background-color:blue; color: white;">Submit</button>
+// JSX  -> <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+// Use className instead of class in JSX to avoid conflict or confusion with declaration.
 const App = () => {
-    return <div>Hi there!</div>;
+    return (
+        <div>
+            <label className="label" for="name">Enter name: </label>
+            <input id="name" type="text"/>
+            <button style={{ backgroundColor: 'red', color: 'white' }}>
+                Submit
+            </button>
+        </div>
+    );
 };
 
 // Take the react component and show it on the screen (DOM)
