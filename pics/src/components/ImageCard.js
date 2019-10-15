188ae0506033aea9d5b436137780cbc6a094df41
keenoro <kkhoo93@gmail.com>
2019-10-15 23:23:05 +1100

    created imagecard file to organize pictures

---
+++ pics/src/components/ImageCard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //React refs system
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  }
+
+  render() {
+    const { description, urls } = this.props.image;
+
+    return (
+      <div>
+        <img 
+          ref={this.imageRef}
+          alt={description} 
+          src={urls.regular} 
+        />
+      </div>
+    )
+  }
+}
+
+export default ImageCard;
