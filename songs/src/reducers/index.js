40ddd60f6395fd11a19dd418601a812ca8b77a1a
keenoro <kkhoo93@gmail.com>
2019-10-18 23:45:30 +1100

    Added actions and reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,17 @@
+// Reducers
+const songsReducer = () => {
+  return [
+    { title: 'Chasing Cars', duration: '3:05'},
+    { title: 'No Scrubs', duration: '4:00'},
+    { title: 'All Star', duration: '3:15'},
+    { title: 'I Want it That Way', duration: '1:40'}
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  
+  return selectedSong;
+};
