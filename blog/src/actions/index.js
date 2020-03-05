17b2fec9478e9e5dbeb0f6c3902f8e155de530d6
keenoro <kkhoo93@gmail.com>
2020-03-05 16:00:12 +1100

    using chaining from lodash

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,23 +1,50 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-// Correct approach! Using redux-thunk as Middleware. And refactored
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  //Compact version with Lodash chain
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
+
+//Correct approach! Using redux-thunk as Middleware. And refactored
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');  
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
-};
 
-  // Bad approach!! Due to async and await, and API time.
+  //Bad approach!! Due to async and await, and API time.
   // const response = await jsonPlaceholder.get('/posts');  
 
   // return {
   //   type: 'FETCH _POSTS',
   //   payload: response
   // };
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
 
 
+//Memoized version with Lodash
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
 
-// This is totally fine, still can make normal action creators
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
+
+//This is totally fine, still can make normal action creators
 // export const selectPost = () => {
 //   return {
 //     type: 'SELECT_POST'
