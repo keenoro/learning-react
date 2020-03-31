ba420f8cbba5a539ec374f29333424c400ff7435
keenoro <kkhoo93@gmail.com>
2020-03-31 18:40:34 +1100

    Streaming works

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -21,6 +21,10 @@ class StreamShow extends React.Component {
     this.buildPlayer();
   }
 
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
   // Check if the player is built or the stream is not fetched
   buildPlayer() {
     if (this.player || !this.props.stream ) {
