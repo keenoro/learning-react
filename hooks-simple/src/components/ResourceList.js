7150a5863063f9eb1461c29c5c813c9d8e2912e5
keenoro <kkhoo93@gmail.com>
2020-04-03 20:43:32 +1100

    Created useResources function to make it reusable

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
+const useResources = (resource) => {
   const [resources, setResources] = useState([]);
 
   // Combines componentDidMount and componentDidUpdate
   // Only runs the arrow function if the [resource] changes
-  useEffect(() => {
-    (async (resource) => {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-      setResources(response.data)
-    })(resource)
-  }, 
-  [resource]);
+  useEffect(
+    () => {
+      (async (resource) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+        setResources(response.data)
+      })(resource)
+    }, 
+    [resource]
+  );
 
-  return <div>{resources.length}</div>
+  return resources;
+}
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
+
+  return (
+    <ul>
+      {resources.map(record => <li key={record.id}>{record.title}</li>)}
+    </ul>
+  )
 }
 
 export default ResourceList;
