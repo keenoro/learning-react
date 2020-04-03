7d5388da8a429e60773ce9f14f52ff88a18d6bd2
keenoro <kkhoo93@gmail.com>
2020-04-03 17:38:24 +1100

    useEffect for lifecycle hooks

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = () => {
+const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async () => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-    this.setState({ resources : response.data });
+  const fetchResource = async (resource) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+    setResources(response.data);
   }
+
+  // Combines componentDidMount and componentDidUpdate
+  // Only runs the arrow function if the [resource] changes
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource]);
 
   return <div>{resources.length}</div>
 }
